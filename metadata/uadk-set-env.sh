b43e0b2d443f279144e36534751a74b3203a1870
Guodong Xu <guodong.xu@linaro.org>
2024-03-29 17:46:34 +0800

    uadk-set-env.sh: updated with both UADK and OpenSSL 3.0 environment settings
    Signed-off-by: Guodong Xu <guodong.xu@linaro.org>

--- metadata/uadk-set-env.sh
+++ metadata/uadk-set-env.sh
@@ -12,13 +12,18 @@
 set -x
 
 # enable HiSilicon accelerator devices read/write permission
-sudo chmod 666 /dev/hisi_*
+sudo chmod 777 /dev/hisi_*
 
 # enable logging through rsyslog
+# UADK log file located: /var/log/uadk.log
+sudo echo "local5.*  /var/log/uadk.log" >> /etc/rsyslog.conf
 sudo /usr/sbin/rsyslogd || true
 
 # set Library path
 export LD_LIBRARY_PATH=/usr/local/lib:${LD_LIBRARY_PATH}
+
+# set pkg-config search path
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig/
 
 # set queue number
 export WD_RSA_CTX_NUM="sync:2@0,async:4@0"
@@ -32,3 +37,6 @@ export WD_DH_EPOLL_EN=1
 export WD_RSA_EPOLL_EN=1
 export WD_ECC_EPOLL_EN=1
 export WD_COMP_EPOLL_EN=1
+
+# OpenSSL 3.0 modules path
+export OPENSSL_MODULES=/usr/local/lib/ossl-modules
